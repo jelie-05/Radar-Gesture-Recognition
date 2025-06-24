6d3dc9a93dea190ea8ddab547a7e8aca630b9b12
jelie-05 <jeremialieks@gmail.com>
2025-06-24 11:19:36 +0200

    cmake

---
+++ cpp_inference/build/CMakeFiles/infer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/infer.dir/main.cpp.o"
+  "CMakeFiles/infer.dir/main.cpp.o.d"
+  "infer"
+  "infer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/infer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
